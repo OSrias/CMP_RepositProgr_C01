94da5b397897b6ac8c43aa8b907f2649f50a7d66
OSrias <97224888+OSrias@users.noreply.github.com>
2022-01-07 11:57:30 +0000

    Add files via upload

---
+++ D12_Coleçoes/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D12_Coleçoes
+{
+    class Employee : Pessoas
+    {
+        #region Properties
+        public string Department { get; set; }
+
+        #endregion
+
+    }
+}
